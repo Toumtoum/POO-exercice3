a3357a61abafb877fe2feb6dd987989ee3d68cf1
Toumtoum <mehdi.toumia@yahoo.com>
2017-08-22 16:59:12 +0200

    creation of 2 object / need onclick event to call methods

--- js/main.js
+++ js/main.js
@@ -1,28 +1,38 @@
-// ---------------------------------VARIABLES-----------------------------------
+function product (title,catchPhrase,description){
 
+  this.title = title,
+  this.catchPhrase = catchPhrase,
+  this.description = description,
 
+ this.display = function () {
 
+  var card = document.getElementById('card').cloneNode(true);
+  document.getElementById('table').appendChild(card);
+  document.getElementById('productTitle').innerHTML = this.title;
+  document.getElementById('productCatchPhrase').innerHTML = this.catchPhrase;
+  document.getElementById('productDescription').innerHTML = this.description;
 
+}
+}
 
+function form (){
 
+this.getInputVal = function () {
 
+  var titleValue = document.getElementById('title').value;
+  var catchPhraseValue = document.getElementById('catchPhrase').value;
+  var descriptionValue = document.getElementById('description').value;
 
+  var newCard = new product (titleValue,catchPhraseValue,descriptionValue);
+  newCard.display ();
 
+},
 
+this.resetInput = function () {
 
+  document.getElementById('title').value = "";
+  document.getElementById('catchPhrase').value = "";
+  document.getElementById('description').value = "";
 
-// ---------------------------------FONCTIONS-----------------------------------
-
-
-
-
-
-
-
-
-
-
-
-
-
-// ----------------------------------SCRIPT-----------------------------------
+}
+}
